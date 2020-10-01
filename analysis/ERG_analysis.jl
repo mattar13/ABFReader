31c59ba8fefb23763c78f09d5dfa663119d2236e
Matthew Tarchick <mtarchick13@gmail.com>
2020-10-01 16:39:29 -0400

    Added analysis Pluto notebook

---
+++ analysis/ERG_analysis.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.11.14
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e09e64b0-0425-11eb-1a08-8f78d2ceca08
+println("Individual analysis will go here")
+
+# ╔═╡ Cell order:
+# ╠═e09e64b0-0425-11eb-1a08-8f78d2ceca08
