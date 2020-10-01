4ace18379779b42249323d31bd328447f56df904
Matthew Tarchick <mtarchick13@gmail.com>
2020-10-01 18:41:24 -0400

    Added clean_data and new analysis file

---
+++ analysis/ERG_concat_analysis.jl
@@ -0,0 +1,64 @@
+### A Pluto.jl notebook ###
+# v0.11.14
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 66f48ca0-0436-11eb-0e18-45060045af67
+using PlutoUI
+
+# ╔═╡ 830b8f10-0436-11eb-3668-a9da07d1ee55
+using NeuroPhys, Plots
+
+# ╔═╡ 87a9cf50-0436-11eb-1695-974c7d2f3298
+pyplot()
+
+# ╔═╡ 8addd130-0436-11eb-2256-6bae5253165e
+target_path = "D:\\Data\\ERG\\Gnat_Group\\2020_09_04_ERG\\Mouse1"
+
+# ╔═╡ 9693dd30-0436-11eb-2285-2bc0ee518909
+begin 
+	paths = target_path |> parse_abf
+	a_paths = String[]
+	#And AB wave traces
+	ab_paths = String[]
+	for path in paths
+		search = (splitpath(path))
+		if length(findall(x -> x == "Drugs", search)) > 0
+			push!(a_paths, path)
+		elseif length(findall(x -> x == "NoDrugs", search)) > 0
+			push!(ab_paths, path)
+		end
+	end	
+end;
+
+# ╔═╡ 9b96aba0-0436-11eb-32b8-2fc4e2f30008
+md"
+Full ERG traces: $(length(ab_paths))
+
+$ab_paths
+
+a-wave ERG traces $(length(a_paths))
+
+$a_paths
+"
+
+# ╔═╡ ab8187b0-0436-11eb-159a-93c1a4f73e13
+begin 
+	import NeuroPhys: concat, clean_data
+	concat_paths = a_paths[1:5];
+	t, concat_data = concat(concat_paths;
+		filter = NeuroPhys.clean_data, t_cutoff = 0.75, t_eff = 0.25);
+	#Normalize data
+	#ch1_norm, norm_factor1 = normalize(concat_data[:,:,1]);
+	#ch2_norm, norm_factor2 = normalize(concat_data[:,:,2]);
+end
+
+# ╔═╡ Cell order:
+# ╠═66f48ca0-0436-11eb-0e18-45060045af67
+# ╠═830b8f10-0436-11eb-3668-a9da07d1ee55
+# ╠═87a9cf50-0436-11eb-1695-974c7d2f3298
+# ╠═8addd130-0436-11eb-2256-6bae5253165e
+# ╟─9693dd30-0436-11eb-2285-2bc0ee518909
+# ╟─9b96aba0-0436-11eb-32b8-2fc4e2f30008
+# ╠═ab8187b0-0436-11eb-159a-93c1a4f73e13
--- src/functions.jl
+++ src/functions.jl
@@ -58,6 +58,25 @@ function fft_spectrum(t, data::Array{T, 1}) where T <: Real
     return freqs[over_0], x_fft[over_0] 
 end
 
+#A single filtering pipeline
+function clean_data(t, data; negative = true, wave = WT.dog2, cutoff_octave = 9)
+    x_ch1 = data[1,:,1]; x_ch2 = data[1,:,2]; x_stim = data[1,:,3] .> 0.2;
+    #Cancelling drift
+    x_lin1 = drift_cancel(t, x_ch1);
+    x_lin2 = drift_cancel(t, x_ch2);
+    #Baseline subtraction
+    stim_idxs = findall(x -> x == true, x_stim) #Stimulus is same for both channels
+    x_adj1 = subtract_baseline(x_lin1, (1, stim_idxs[1]));
+    x_adj2 = subtract_baseline(x_lin2, (1, stim_idxs[1]));
+    #Normalization
+    x_norm1, norm_factor1 = normalize(x_adj1);
+    x_norm2, norm_factor2 = normalize(x_adj2);
+    #CWT filtering (Probably not ready for CWT filtering )
+    x_cwt1, cwt1_raster = cwt_filter(x_norm1, periods = 1:cutoff_octave);
+    x_cwt2, cwt2_raster = cwt_filter(x_norm2, periods = 1:cutoff_octave);
+    return x_cwt1.*norm_factor1, x_cwt2.*norm_factor2, x_stim
+end
+
 #########################################Everything Below here is for Pepperburg analysis
 """
 This function normalizes data and sets the minimum of the highest intensity nose component
