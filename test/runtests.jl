e87228b9711cd7391815d62afc5f05277155817c
Matthew Tarchick <mtarchick13@gmail.com>
2020-10-03 18:57:17 -0400

    Added two tests for exporting files

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,14 @@
 #test to see if the functions are working
 using NeuroPhys
+println("Package properly exported")
+
+#%% Test the exporting and filtering of .abf files
+t, filter_data, dt = extract_abf("to_filter.abf")
+println("properly imported data")
+println(t)
+println(dt)
+
+t, analyze_data, dt = extract_abf("to_analyze.abf")
+println("properly imported data")
+println(t)
+println(dt)
