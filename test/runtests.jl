1905a0e79f07b72a1c3b4635754b4416236cf32c
Matthew Tarchick <mtarchick13@gmail.com>
2020-11-30 13:11:55 -0500

    editing runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,20 @@
-#test to see if the functions are working
+#%%
+using Pkg
+Pkg.update("NeuroPhys")
+#%%test to see if the functions are working
+using Revise
 using NeuroPhys
+using Plots
 println("Package properly exported")
 
 #%% Test the exporting and filtering of .abf files
-t, filter_data, dt = extract_abf("to_filter.abf")
-println("properly imported data")
-println(dt)
+target_path = "to_filter.abf"
 
-t, analyze_data, dt = extract_abf("to_analyze.abf")
-println("properly imported data")
-println(dt)
+trace = extract_abf(target_path); #Extract the data
+drift_trace = baseline_cancel(trace; mode = :slope, region = :prestim) #Cancel drift
+baseline_trace = baseline_cancel(drift_trace; mode = :mean, region = :prestim) #Baseline data
+filter_trace = lowpass_filter(baseline_trace) #Lowpass filter using a 40hz 8-pole filter
+cwt_trace = cwt_filter(baseline_trace) #Use a continuous wavelet transform to remove noise, but keep time series info
+
+
+#%% Test the analysis of .abf files
