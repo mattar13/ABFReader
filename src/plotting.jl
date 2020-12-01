97aeefb5272462c23f5927cfa406f4e3250c4b1b
Matthew Tarchick <mtarchick13@gmail.com>
2020-12-01 13:23:24 -0500

    color issue

--- src/plotting.jl
+++ src/plotting.jl
@@ -20,7 +20,7 @@ This function plots by channel. This is the most basic functionality of the trac
             @series begin
                 t_stim_start, t_stim_end = findstimRng(nt)
                 subplot := i
-                seriescolor = :yellow
+                color = :yellow
                 seriestype := :vline
                 label := "Stimulus"
                 y := [nt.t[t_stim_end]]
