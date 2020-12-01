a2960ddda38eed21b3faadde09aebda6da097b00
Matthew Tarchick <mtarchick13@gmail.com>
2020-12-01 13:19:55 -0500

    reorganize

--- src/plotting.jl
+++ src/plotting.jl
@@ -1,29 +1,32 @@
 """
 This function plots by channel. This is the most basic functionality of the trace plot
 """
-@recipe function f(nt::NeuroTrace; stim_plot = :subplot)
+@recipe function f(nt::NeuroTrace; stim_plot = :subplot, c = :blue)
     grid := false
     layout := (size(nt,3), 1)
+    
     if stim_plot == :include
         layout := (size(nt,3)-1, 1)
         for (i,ch) in enumerate(eachchannel(nt; include_stim = false))
             @series begin
                 subplot := i
+                c := c
                 x := nt.t
-                y := ch
-                yguide := "$(nt.chNames[i])($(nt.chUnits[i]))"
                 if i == size(nt,3)
                     xguide := "Time ($(nt.tUnits))" 
                 end
+                y := ch
+                yguide := "$(nt.chNames[i])($(nt.chUnits[i]))"
                 ()
             end
             @series begin
                 t_stim_start, t_stim_end = findstimRng(nt)
                 subplot := i
-                seriestype := :vline
                 c = :yellow
+                seriestype := :vline
                 label := "Stimulus"
                 y := [nt.t[t_stim_end]]
+                yguide := "$(nt.chNames[i])($(nt.chUnits[i]))"
             end
         end
     else
