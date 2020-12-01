efd33b0ee332f45c63d5b6f97d0c8cd584cbf090
Matthew Tarchick <mtarchick13@gmail.com>
2020-12-01 14:24:58 -0500

    plotting adjust

--- src/plotting.jl
+++ src/plotting.jl
@@ -23,20 +23,20 @@ This function plots by channel. This is the most basic functionality of the trac
                 seriestype := :vline
                 label := "Stimulus"
                 y := [nt.t[t_stim_end]]
-                #yguide := "$(nt.chNames[i])($(nt.chUnits[i]))"
+                ()
             end
         end
     else
         layout := (size(nt,3), 1)
         for (i,ch) in enumerate(eachchannel(nt))
+            xlabels = reshape(repeat([""], size(nt,3)), (1, size(nt,3)))
+            xlabels[end] = "Time ($(nt.tUnits))"
+            xguide := "Time ($(nt.tUnits))" 
             @series begin
                 subplot := i
                 x := nt.t
                 y := ch
                 yguide := "$(nt.chNames[i])($(nt.chUnits[i]))"
-                if i == size(nt,3)
-                    xguide := "Time ($(nt.tUnits))" 
-                end
                 ()
             end
         end
