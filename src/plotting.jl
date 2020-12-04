1f95ae1a6475688171ca0c8fea88014cb66a1ab0
Matthew Tarchick <mtarchick13@gmail.com>
2020-12-04 12:17:16 -0500

    fixed plotting bug

--- src/plotting.jl
+++ src/plotting.jl
@@ -28,7 +28,7 @@ This function plots by channel. This is the most basic functionality of the trac
                     ()
                 end
                 @series begin
-                    trng = findstimRng(data) .* data.dt
+                    trng = findstimRng(nt) .* nt.dt
                     stim_duration = -(trng...) * 1000
                     subplot := ch
                     seriescolor := :yellow
