59dbb623cfa17b2b02e8f488ba5ee1bdb8f2306d
Matthew Tarchick <mtarchick13@gmail.com>
2021-01-04 19:19:56 -0500

    updated the data analysis section

--- notebooks/analyzing_data_script.jl
+++ notebooks/analyzing_data_script.jl
@@ -6,8 +6,6 @@ using StatsBase, Statistics
 #%% Recapitulating Pauls data
 target_folder = "D:\\Data\\ERG\\Data from paul\\"
 paths = target_folder |> parse_abf
-println(length(paths))
-#%%
 #Pauls data is in one of several different formats
 format1 = ("\\", ~, ~, ~, ~, ("_", (" ", ~, :Rearing, check_pc), :Sample_size), check_color, :Drugs, ("_", :Month, :Day, :Year, :Genotype, check_age, :Animal, ~,~,~))
 format2 = ("\\", ~, ~, ~, ~, ("_", (" ", ~, :Rearing, check_pc), :Sample_size), check_color, :Drugs, ("_", :Month, :Day, :Year, :Genotype, check_age, :Animal, ~))
@@ -54,14 +52,15 @@ for (i,path) in enumerate(paths)
                 Photoreceptors = "Both"
             end
         end
-
+        
+        t_pre = 1.0
         if Photoreceptors == "cones" || Photoreceptors == "Both"
-            t_pre = 1.0
-            t_post = 1.0
+            #Cone responses are under 300ms
+            t_post = 0.3
             saturated_thresh = Inf
         else
-            t_pre = 0.5
-            t_post = 3.0
+            #Rod Responses can last a bit longer, so a second is fine for the max time
+            t_post = 1.0
             saturated_thresh = :determine
         end
 
@@ -120,11 +119,11 @@ data_analysis  = data_analysis |> @orderby_descending(_.Rearing) |> @thenby(_.Ag
 
 #paths[fail_files]
 
-#Make and export the dataframe 
+#%% Make and export the dataframe 
 all_categories = data_analysis |> 
-    @unique({_.Age, _.Wavelength, _.Photoreceptors, _.Rearing}) |> 
-    @filter(_.Drugs != "b_waves") |> 
-    @map({_.Age, _.Photoreceptors, _.Wavelength, _.Rearing}) |>
+    #@filter(_.Drugs != "b-waves") |> 
+    @unique({_.Age, _.Wavelength, _.Drugs, _.Photoreceptors, _.Rearing}) |> 
+    @map({_.Age, _.Photoreceptors, _.Drugs, _.Wavelength, _.Rearing}) |>
     DataFrame
 
 rmaxes = Float64[]
@@ -134,16 +133,16 @@ rdims_sem = Float64[]
 tpeaks = Float64[]
 tpeaks_sem = Float64[]
 for row in eachrow(all_categories)
-    println(row.Age)
+    println(row)
     Qi = data_analysis |>
         @filter(_.Age == row.Age) |>
         @filter(_.Photoreceptors == row.Photoreceptors) |> 
-        @filter(_.Rearing == row.Rearing) |> 
-        @filter(_.Rmax < 1000 && _.Rmax > 0) |> 
+        @filter(_.Drugs != "b-waves") |> 
+        @filter(_.Rearing == row.Rearing) |>
         @filter(_.Wavelength == row.Wavelength) |> 
         @map({_.Path, _.Age, _.Wavelength, _.Photoreceptors, _.Rearing, _.Rmax, _.Rdim, _.t_peak}) |> 
         DataFrame
-    println(Qi)
+    #println(Qi)
     rmax_mean = sum(Qi.Rmax)/length(eachrow(Qi))
     rmax_sem = std(Qi.Rmax)/(sqrt(length(eachrow(Qi))))
 
@@ -169,15 +168,28 @@ all_categories[:, :Rdim] = rdims
 all_categories[:, :Rdim_SEM] = rdims_sem
 all_categories[:, :T_Peak] = tpeaks
 all_categories[:, :T_Peak_SEM] = tpeaks_sem
-all_categories  = all_categories |> @orderby_descending(_.Rearing) |> @thenby(_.Age) |> @thenby_descending(_.Photoreceptors) |> @thenby(_.Wavelength) |> DataFrame
+all_categories  = all_categories |> @orderby_descending(_.Rearing) |> @thenby(_.Drugs) |> @thenby(_.Age) |> @thenby_descending(_.Photoreceptors) |> @thenby(_.Wavelength) |> DataFrame
+#%%
+a_wave = data_analysis |> @filter(_.Drugs == "a-waves") |> DataFrame
+b_wave = data_analysis |> @filter(_.Drugs == "b-waves") |> DataFrame
 
+#%% If there is something that is a cause for concern, put it here
+concern = data_analysis |> 
+    @filter(_.Age == 30) |> 
+    @filter(_.Photoreceptors == "cones") |> 
+    @filter(_.Drugs == "a-waves") |> 
+    #@filter(_.Rearing == "(DR)") |> 
+    DataFrame
 #%% Save data
 save_path = joinpath(target_folder,"data.xlsx")
 try
     XLSX.writetable(save_path, 
         #Summary = (collect(eachcol(summary_data)), names(summary_data)), 
-        Full_Data = (collect(eachcol(data_analysis)), names(data_analysis)), 
+        Full_Data = (collect(eachcol(data_analysis)), names(data_analysis)),
+        A_Waves =  (collect(eachcol(a_wave)), names(a_wave)),
+        B_Waves =  (collect(eachcol(b_wave)), names(b_wave)),
         All_Categories = (collect(eachcol(all_categories)), names(all_categories)),
+        Concern = (collect(eachcol(concern)), names(concern))
         #Stats = (collect(eachcol(stats_data)), names(stats_data))
     )
 catch
@@ -185,8 +197,11 @@ catch
     rm(save_path)
     XLSX.writetable(save_path, 
         #Summary = (collect(eachcol(summary_data)), names(summary_data)), 
-        Full_Data = (collect(eachcol(data_analysis)), names(data_analysis)), 
+        Full_Data = (collect(eachcol(data_analysis)), names(data_analysis)),
+        A_Waves =  (collect(eachcol(a_wave)), names(a_wave)),
+        B_Waves =  (collect(eachcol(b_wave)), names(b_wave)) ,
         All_Categories = (collect(eachcol(all_categories)), names(all_categories)),
+        Concern = (collect(eachcol(concern)), names(concern))
         #Stats = (collect(eachcol(stats_data)), names(stats_data))
     )
 end
@@ -194,7 +209,7 @@ end
 
 #%% Plot anything that seems odd (single file plotting)
 #path = paths[fail_files[4]]
-path = "D:\\Data\\ERG\\Data from paul\\P8 (NR)_10\\a-waves\\Green\\4_6_19_WT_P8_m1_ND_Green.abf"
+path = "D:\\Data\\ERG\\Data from paul\\Adult (DR) cones_16\\Green\\a-waves\\11_21_19_DR_P37_m1_D_Cones_Green.abf"
 println(path)
 data_ex = try
     #Some files may have a stimulus channel
@@ -203,31 +218,46 @@ catch
     #Some files may not
     extract_abf(path; stim_ch = -1, swps = -1, chs = -1)
 end
-#using Plots
+#Readout of all the categories of the file
 nt = formatted_split(path, format1, format2, format3, format4, format5, format6, format7)
-#%%
-truncate_data!(data_ex; t_pre = 0.2, t_post = 1.0)
+if nt.Age == 8 || nt.Age == 9
+    println("Photoreceptors equals both")
+    Photoreceptors = "Both"
+else
+    if haskey(nt, :Photoreceptors)
+        Photoreceptors = nt.Photoreceptors
+    else
+        println("No key equaling Photoreceptors")
+        Photoreceptors = "Both"
+    end
+end
+
+t_pre = 0.2
+if Photoreceptors == "cones" || Photoreceptors == "Both"
+    t_post = 0.3
+    saturated_thresh = Inf
+else
+    t_post = 1.0
+    saturated_thresh = :determine
+end
+
+truncate_data!(data_ex; t_pre = t_pre, t_post = t_post)
 filter_data = lowpass_filter(data_ex) #Lowpass filter using a 40hz 8-pole 
-rmaxes = saturated_response(filter_data; saturated_thresh = saturated_thresh)
-rdims, dim_idx = dim_response(filter_data, rmaxes)
-t_peak = time_to_peak(data_ex, dim_idx)
-println(t_peak)
-t_dom = pepperburg_analysis(data, rmaxes)
+
 if findstimRng(data_ex)[1] == 1
     println("Don't baseline")
 else
     baseline_cancel!(data_ex) #Baseline data
 end
 
-filter_data = lowpass_filter(data_ex) #Lowpass filter using a 40hz 8-pole 
-rmaxes = saturated_response(filter_data; saturated_thresh = Inf)
-ppbg_thresh = rmaxes .* 0.60;
+rmaxes = saturated_response(filter_data; saturated_thresh = saturated_thresh)
 rdims, dim_idx = dim_response(filter_data, rmaxes)
 t_peak = time_to_peak(data_ex, dim_idx)
-println(t_peak)
+ppbg_thresh = rmaxes .* 0.60;
 t_dom = pepperburg_analysis(data_ex, rmaxes)
 
-fig1 = plot(data_ex, label = "", title = data_ex.ID, c = :black)
+#data_ex = filter_data
+fig1 = plot(data_ex, label = "", title = data_ex.ID, layout = grid(2,1), c = :black)
 if dim_idx[1] != 0
     plot!(fig1[1], data_ex.t, data_ex[dim_idx[1], :, 1], c = :red)
 end
@@ -236,14 +266,15 @@ if dim_idx[2] != 0
     plot!(fig1[2], data_ex.t, data_ex[dim_idx[2], :, 2], c = :red)
 end
 
-hline!(fig1[1], [rmaxes[1]], c = :green, label = "Rmax")
-hline!(fig1[2], [rmaxes[2]], c = :green, label = "Rmax")
+#hline!(fig1[1], [rmaxes[1]], c = :green, label = "Rmax")
+#hline!(fig1[2], [rmaxes[2]], c = :green, label = "Rmax")
+#Plot rdim thresholds
 hline!(fig1[1], [rdims[1]], c = :red, label = "Rdim")
 hline!(fig1[2], [rdims[2]], c = :red, label = "Rdim")
 vline!(fig1[1], [t_peak[1]], c = :blue, label = "Tpeak")
 vline!(fig1[2], [t_peak[2]], c = :blue, label = "Tpeak")
-plot!(fig1[1], t_dom[:,1], repeat([ppbg_thresh[1]], size(data_ex,1)), marker = :square, c = :grey, label = "Pepperburg", lw = 2.0)
-plot!(fig1[2], t_dom[:,2], repeat([ppbg_thresh[2]], size(data_ex,1)), marker = :square, c = :grey, label = "Pepperburg", lw = 2.0)
+#plot!(fig1[1], t_dom[:,1], repeat([ppbg_thresh[1]], size(data_ex,1)), marker = :square, c = :grey, label = "Pepperburg", lw = 2.0)
+#plot!(fig1[2], t_dom[:,2], repeat([ppbg_thresh[2]], size(data_ex,1)), marker = :square, c = :grey, label = "Pepperburg", lw = 2.0)
 
 #%%
 Qgraph = data_analysis |>
