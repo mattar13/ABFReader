d1fd083e32591ae501579434d60224712850e6ee
Matthew Tarchick <mtarchick13@gmail.com>
2021-02-05 16:01:44 -0500

    changed plotting function

--- src/plotting.jl
+++ src/plotting.jl
@@ -1,67 +1,57 @@
 """
-This function plots by channel. This is the most basic functionality of the trace plot
+Plotting function. 
+
+    sweep_placement = 
+    - :single -> All sweeps plotted on a single plot
+    - :individual -> all sweeps plotted on a individual plot
+    plot_stim_mode:
+    - :overlay_vline_start -> Beginning of stimuli are plotted as a line
+    - :overlay_vline_end -> End of stimuli is plotted as a line
+    - :overlay_vspan -> 
 """
-@recipe function f(nt::Experiment; stim_plot = :include, time_adjusted = true)
+@recipe function f(exp::Experiment; sweep_placement = :single, plot_stim_mode = :overlay_vline_start)
     grid := false
-    if nt.stim_ch == -1
-        #If there is no stimulus, always use the subplot
-        stim_plot = :subplot
+    if sweep_placement == :single 
+        #Place all the sweeps in a single plot
+        layout := (size(exp,3), 1)
+    elseif sweep_placement == :individual
+        println("Not quite ready yet")
+        layout := (size(exp,1), size(exp,3))
     end
 
-    if stim_plot == :include
-        layout := (size(nt,3)-1, 1)
-
-        for swp in 1:size(nt,1)
-            for ch in 1:size(nt,3)-1
-
-                if time_adjusted 
-                    t_series = nt.t .- nt.t[findstimRng(nt)[swp, 1]]
-                else
-                    t_series = nt.t
-                end
-                xlabels = reshape(repeat([""], size(nt,3)-1), (1, size(nt,3)-1))
-                xlabels[end] = "Time ($(nt.tUnits))"
-                xguide := xlabels
-                @series begin
-                    subplot := ch
-                    x := nt.t #t_series
-                    y := nt[swp, :, ch]
-                    yguide := "$(nt.chNames[ch])($(nt.chUnits[ch]))"
-                    ()
-                end
-                @series begin
-                    subplot := ch
-                    seriescolor := :yellow
-                    linewidth := 2.0
-                    seriestype := :vline
-                    if swp == 1
-                        label := "Stimulus"
-                    end
-                    y := [0.0]
-                    yguide := "$(nt.chNames[ch])($(nt.chUnits[ch]))"
-                    ()
-                end
-            end
+    for swp in 1:size(exp,1), ch in 1:size(exp,3)
+        xlabels = reshape(repeat([""], size(exp,3)-1), (1, size(exp,3)-1))
+        xlabels[end] = "Time ($(exp.tUnits))"
+        xguide := xlabels
+        @series begin
+            label := ""
+            subplot := ch
+            x := exp.t #t_series
+            y := exp[swp, :, ch]
+            yguide := "$(exp.chNames[ch])($(exp.chUnits[ch]))"
+            ()
         end
-    else
-        layout := (size(nt,3), 1)
-        for swp in 1:size(nt,1)
-            for ch in 1:size(nt,3)
-                if time_adjusted 
-                    t_series = nt.t .- nt.t[findstimRng(nt)[swp, 1]]
-                else
-                    t_series = nt.t
-                end
-                xlabels = reshape(repeat([""], size(nt,3)), (1, size(nt,3)))
-                xlabels[end] = "Time ($(nt.tUnits))"
-                xguide := "Time ($(nt.tUnits))" 
-                @series begin
-                    subplot := ch
-                    x := nt.t #t_series
-                    y := nt[swp, :, ch]
-                    yguide := "$(nt.chNames[ch])($(nt.chUnits[ch]))"
-                    ()
-                end
+        @series begin
+            subplot := ch
+            seriescolor := :yellow
+            linewidth := 2.0
+            label := ""
+            if plot_stim_mode == :overlay_vline_start
+                seriestype := :vline
+                y := [exp.stim_protocol[swp].timestamps[1]]
+                yguide := "$(exp.chNames[ch])($(exp.chUnits[ch]))"
+                ()
+            elseif plot_stim_mode == :overlay_vline_end
+                seriestype := :vline
+                y := [exp.stim_protocol[swp].timestamps[2]]
+                yguide := "$(exp.chNames[ch])($(exp.chUnits[ch]))"
+                ()
+            elseif plot_stim_mode == :overlay_vspan
+                alpha := 0.2
+                seriestype := vspan
+                y := [exp.stim_protocol[swp].timestamps...]
+                yguide := "$(exp.chNames[ch])($(exp.chUnits[ch]))"
+                ()
             end
         end
     end
