679cf1c398d4c7306aa64cac40b0c1997abff69a
Matthew Tarchick <mtarchick13@gmail.com>
2021-06-01 16:56:08 -0400

    added condition check

--- src/file_formatting.jl
+++ src/file_formatting.jl
@@ -185,6 +185,7 @@ end
 
 ########################### These are some functions that will make parsing folder names easier ##############
 function contiditon_check(x::String)
+    #Don't parse the conditions as numbers
     return (:Conditions, x)
 end
 
