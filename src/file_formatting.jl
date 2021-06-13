9dff8bc28d062eb0f04fad7a134a5282045160bd
Matthew Tarchick <mtarchick13@gmail.com>
2021-06-13 17:52:05 -0400

    fixed mistake in filename_formatting

--- src/file_formatting.jl
+++ src/file_formatting.jl
@@ -206,9 +206,11 @@ function choose_filename(x::String)
     #We want to skip files containing concatenate but pass files containing average only
     t1 = contains_words(x; words = ["CONCATENATE"], result = :fail)
 	t2 = contains_words(x; words = ["AVERAGE"], result = :pass)
-    if !isnothing(t1) || !isnothing(t2)
-        println("returning")
+    if !isnothing(t1) 
         return t1 #no need to return both
+    end
+    if !isnothing(t2)
+        return t2
     end
 end
 
