68a0ced152db3b8594272fff7c5866368941f96c
Matthew Tarchick <mtarchick13@gmail.com>
2021-07-01 15:23:28 -0400

    added and updated logging

--- src/logging.jl
+++ src/logging.jl
@@ -5,13 +5,28 @@ dotenv(env_location) #First we can load the .env file
 #this variable will be added
 function BotNotify(text::String; add_date = true)
      if add_date
-          println("[$(now())] : \n $text")
-          sendMessage(text = "[$(now())] : \n $text")
+          println("[$(now())]: $text")
+          sendMessage(text = "[$(now())]: $text")
      else
           println(text)
           sendMessage(text = text) 
      end
      return nothing
-end\
+end
+
+function wait_for_response()
+     #We are going to run the bot until a message has been recieved
+     run_bot() do msg
+          response = msg.message.text
+          println(lowercase(response))
+          if lowercase(response) == "exit"
+               println("Exiting loop")
+               #We might need to do something weird like throw an error
+               @assert 1 == 2 "InterruptException"
+          else
+               println(response)
+          end
+     end
+end
 
 
