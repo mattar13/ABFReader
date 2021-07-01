b0ff26b86f7f29effec30ad721c2baa46dfcc942
Matthew Tarchick <mtarchick13@gmail.com>
2021-07-01 15:02:44 -0400

    added a next line in JuliaNotifier

--- src/logging.jl
+++ src/logging.jl
@@ -5,11 +5,13 @@ dotenv(env_location) #First we can load the .env file
 #this variable will be added
 function BotNotify(text::String; add_date = true)
      if add_date
-          println("$(now()) $text")
-          sendMessage(text = "$(now()) $text")
+          println("[$(now())] : \n $text")
+          sendMessage(text = "[$(now())] : \n $text")
      else
           println(text)
           sendMessage(text = text) 
      end
      return nothing
-end
+end\
+
+
