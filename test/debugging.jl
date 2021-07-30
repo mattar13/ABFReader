a29971636d1fd027c5b65fa45cef575d5dfb08f8
Matthew Tarchick <mtarchick13@gmail.com>
2021-07-30 12:37:07 -0400

    updated debugging

--- test/debugging.jl
+++ test/debugging.jl
@@ -3,18 +3,18 @@ using Revise
 using NeuroPhys
 using DataFrames, XLSX, Query, Statistics, StatsBase
 dotenv("D\\.env")
+using Dates
+
 
 #%% Can we extract the ABF file details?
 filename = "E:\\Data\\ERG\\Gnat\\2021_06_24_ERG_GNAT\\Mouse1_Adult_GNAT-KO\\BaCl\\Green\\nd1_1p_0001.abf"
 header_info = scanABF(filename)
-header_info["uProtocolPathIndex"]
-#%%
-#The section is divided into [blockStart, entrySize, entryCount]
+header_info["FileStartDateTime"]
+#%% The section is divided into [blockStart, entrySize, entryCount]
+
 blockStart, entrySize, entryCount = header_info["StringsSection"]
 strings = readStringSection(filename, blockStart, entrySize, entryCount)
-#%%
-strings[1]
-#%%
+#%% Extract the strings
 internal_strings = split(strings[1], "\00")
 internal_strings = internal_strings[internal_strings.!=""]
 internal_strings = internal_strings[5:end]
