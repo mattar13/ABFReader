54617293eee9839a058235b5947827bd484dca8f
Matthew Tarchick <mtarchick13@gmail.com>
2021-08-10 02:35:21 -0400

    updated debugging

--- test/debugging.jl
+++ test/debugging.jl
@@ -3,54 +3,27 @@ using NeuroPhys
 using DataFrames, XLSX, Query, Statistics, StatsBase
 dotenv("D\\.env")
 using Dates, Plots
+
 #%% Extract the abf file details
 filename = "test\\to_analyze.abf"
-@time header_info = parseABF(filename)
-header_info["Epochs"] 
-epochWaveformsBySweep = NeuroPhys.makeEpochTable(header_info, 1)
-epochWaveformsBySweep
-#header_info["EpochTable"] 
-header_info["EpochSection"]
-# we should make a epoch table dataframe
-epochs = header_info["Epochs"]
+exp = readABF(filename; average_sweeps = true)
 
-fnames = fieldnames(header_info["Epochs"][1] |> typeof)
-fvals = map(nm -> getfield(header_info["Epochs"][1], nm), fnames)
-nt = NamedTuple{fnames}(fvals)
-DataFrame([nt])
+#if we need to open the ABF use this function
+@time abfInfo = NeuroPhys.readABFHeader(filename)
+data = NeuroPhys.getWaveform(abfInfo, ["Vm_prime","Vm_prime4", "IN 7"])
+d0 = NeuroPhys.getWaveform(abfInfo, "D0")
 
-header_info["EpochPerDACSection"]["nEpochNum"]
-header_info["DACSection"]["nWaveformEnable"]
-header_info["DACSection"]["nWaveformSource"]
-#%% we want to go through and add only the most necessary things to the experiment object
-readABF(filename; average_sweeps = true)
-
+plot(d0[1,:], xlims = (73100, 73200))
+plot!(data[1,:,3])
+epochTable = abfInfo["EpochTableByChannel"][1]
 #%%
-epochLetter = String[]
-num = 3
-while num >= 0
-     println(num)
-     push!(epochLetter, Char(num % 26 + 65) |> string)
-     num -= 26
-end
-join(epochLetter)
-#%% Lets actually see what python determines for each category
-using PyCall
-pyABF = pyimport("pyabf")
-trace_file = pyABF.ABF(filename)
-members = PyCall.inspect[:getmembers](trace_file)
-m_names = map(m -> m[1], members)
-m_vals = map(m -> m[2], members)
-m_dict = Dict(zip(m_names, m_vals))
-m_dict["data"]
+openABF(filename)
+#%% we want to go through and add only the most necessary things to the experiment object
+
 
 #%%
 data_old = extract_abf(filename)
 data_old[1, :, 1]
-
-#%%
-mycmd = `explorer.exe $(filename)`
-run(mycmd)
 
 #%% Lets make a dataframe that does not alter the other dataframe categories
 root1 = "E:\\Data\\ERG\\Gnat\\"
