0c9ba4e951a2e4182ee7b4b3de10f91664c426cc
Matthew Tarchick <mtarchick13@gmail.com>
2021-08-10 00:30:43 -0400

    completed all convienance functions

--- src/abf_extraction.jl
+++ src/abf_extraction.jl
@@ -427,55 +427,117 @@ function getDigitalWaveform(e::EpochSweepWaveform, channel)
 end
 
 """
-This function retrieves the file by name rather than by number
+    getWaveform(abfInfo::Dict{String, Any}, sweep, channel; channel_type) 
+
+extracts a waveform from the data, digital stimuli, or analog stimuli
+
+...
+#Arguments
+    NEEDED: 
+    -'abfInfo': A dictionary containing all info parsed from the the .abf file
+    -'sweep': sweep/sweeps that will be read from the data or stimuli
+        This is a Int64 number or a list of numbers that represents the index/indexes of the sweep
+    -'channel': The channel which is set to be the explicit stimulus 
+        This can either be a channel index, or a string
+    OPTIONS:
+    -'channel_type': This represents where the waveform will come from. There are 3 options:
+        data -> this is the actual data of the file
+        analog -> this is the analog stimulus of the file
+        digital -> this is the digital stimulus of the file
+...
 """
-function getWaveform(abf::Dict{String, Any}, channel::Real, sweep::Real; channel_type = :analog)
-   if channel_type == :digital
+function getWaveform(abf::Dict{String, Any}, sweep::Int64, channel::Int64; channel_type = :analog)
+    if channel_type == :data
+        #rather than extracting the digital or analog stimuli, we use the actual ADC
+        return abf["data"][sweep, :, channel]
+    elseif channel_type == :analog
+        epochTable = abf["EpochTableByChannel"][channel] #Load the channel
+        epoch = epochTable.epochWaveformBySweep[sweep] #Load the sweep
+        return getAnalogWaveform(epoch)
+    elseif channel_type == :digital
         activeDACch = abf["ProtocolSection"]["nActiveDACChannel"]+1
         epochTable = abf["EpochTableByChannel"][activeDACch] #Load the location of the digital stim
         epoch = epochTable.epochWaveformBySweep[sweep] #Load the sweep
         return getDigitalWaveform(epoch, channel) #Load the digital channel
         #the analog channel containing the data is located here
-    elseif channel_type == :analog
-        epochTable = abf["EpochTableByChannel"][channel] #Load the channel
-        epoch = epochTable.epochWaveformBySweep[sweep] #Load the sweep
-        return getAnalogWaveform(epoch)
     end
 end
 
-function getWaveform(abf::Dict{String, Any}, channel::String, sweep::Real)
-    channel_ID = lowercase(channel[1:end-1])
-    channel_ID = split(channel_ID, " ")[1] |> string
-    channel_num = parse(Int64, channel[end])+1
-    if channel_ID == "d" || channel_ID == "dig" || channel_ID == "digital"
-        return getWaveform(abf, channel_num, sweep; channel_type = :digital)
-    elseif channel_ID == "an" || channel_ID == "a" || channel_ID == "ana" ||channel_ID == "analog"
-        return getWaveform(abf, channel_num, sweep)
+function getWaveform(abf::Dict{String, Any}, sweep::Int64, channels::Vector{T}; kwargs...) where T
+    waveforms = zeros(1, abf["sweepPointCount"], channels|>length)
+    for (i, channel) in enumerate(channels)
+        waveforms[1, :, i] = getWaveform(abf, sweep, channel; kwargs...)
+    end
+    return waveforms
+end
+
+function getWaveform(abf::Dict{String, Any}, sweeps::Vector{Int64}, channel::T; kwargs...) where T
+    waveforms = zeros(sweeps|>length, abf["sweepPointCount"], 1)
+    for (i, sweep) in enumerate(sweeps)
+        waveforms[i, :, 1] = getWaveform(abf, sweep, channel; kwargs...)
+    end
+    return waveforms
+end
+
+#In this case, channels can come as a string or a int, it will get passes to the correct place
+function getWaveform(abf::Dict{String, Any}, sweeps::Vector{Int64}, channels::Vector{T}; kwargs...) where T
+    waveforms = zeros(sweeps|>length, abf["sweepPointCount"], channels|>length)
+    for (i, sweep) in enumerate(sweeps), (j, channel) in enumerate(channels)
+        waveforms[i, :, j] .= getWaveform(abf, sweep, channel; kwargs...)
+    end
+    return waveforms
+end
+
+function getWaveform(abf::Dict{String, Any}, sweep::Union{Vector{Int64}, Int64}, channel::String)
+    #first we check to see if the channel name is in the adc names
+    data_idx = findall(x -> channel == x,  abf["adcNames"])
+    if !isempty(data_idx)
+        return getWaveform(abf, sweep, data_idx[1]; channel_type = :data)
     else
-        println("Please use one of these formats: ̸
-            1) Digital: [D, Dig, Digital]
-            2) Analog: [A, An, Ana, Analog]
-        ")  
-        throw("Improper channel ID")
+        channel_ID = lowercase(channel[1:end-1])
+        channel_ID = split(channel_ID, " ")[1] |> string
+        channel_num = parse(Int64, channel[end])+1
+        if channel_ID == "d" || channel_ID == "dig" || channel_ID == "digital"
+            return getWaveform(abf, sweep, channel_num; channel_type = :digital)
+        elseif channel_ID == "an" || channel_ID == "a" || channel_ID == "ana" ||channel_ID == "analog"
+            return getWaveform(abf, sweep, channel_num)
+        else
+            println("Please use one of these formats: ̸
+                1) Digital: [D, Dig, Digital]
+                2) Analog: [A, An, Ana, Analog]
+            ")  
+            throw("Improper channel ID")
+        end
     end
 end
+
 
 function getWaveform(abf::Dict{String, Any}, channel::String)
     #This function gets called to iterate through all sweeps
     waveforms = zeros(abf["sweepCount"], abf["sweepPointCount"])
     for i in 1:abf["sweepCount"]
-        waveforms[i, :] = getWaveform(abf, channel, i)
+        waveforms[i, :] .= getWaveform(abf, i, channel)
     end
     return waveforms
 end
 
-function getWaveform(abf::Dict{String, Any}, channel::Real; channel_type = :analog)
-    waveforms = zeros(abf["sweepCount"], abf["sweepPointCount"])
-    for i in 1:abf["sweepCount"]
-        waveforms[i, :] = getWaveform(abf, channel, i; channel_type = channel_type)
+function getWaveform(abf::Dict{String, Any}, channels::Vector{String})
+    #This function gets called to iterate through all sweeps
+    waveforms = zeros(abf["sweepCount"], abf["sweepPointCount"], length(channels))
+    for i in 1:abf["sweepCount"], (j, channel) = enumerate(channels)
+        waveforms[i, :, j] .= getWaveform(abf, i, channel)
     end
     return waveforms
 end
+
+function getWaveform(abf::Dict{String, Any}, channel::Int64; kwargs...)
+    waveforms = zeros(abf["sweepCount"], abf["sweepPointCount"])
+    for i in 1:abf["sweepCount"]
+        waveforms[i, :] = getWaveform(abf, i, channel; kwargs...)
+    end
+    return waveforms
+end
+
 """
 These functions handle the byte interpretations of the ABF file
 
@@ -1005,7 +1067,7 @@ function readABF(::Type{T}, abf_path::String;
         stim_name = ["IN 7"], 
         stim_threshold::T = 0.2,
         keep_stimulus_channel::Bool = false,
-        continuous::Bool = false, #this can be achieved in gap free mode
+        continuous::Bool = false, #this can be achieved in gap free mode, I will work on that next
         verbose::Bool = false
     ) where T <: Real
     abf_info = parseABF(abf_path)
