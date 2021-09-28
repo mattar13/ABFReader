9616caf06d0a38cfdaf492451d1432778a11336e
Matthew Tarchick <mtarchick13@gmail.com>
2021-09-27 20:33:03 -0400

    fixed some stuff in fft_spectrum

--- src/filtering.jl
+++ src/filtering.jl
@@ -259,11 +259,11 @@ function fft_spectrum(data::Experiment)
     dt = t[2] - t[1]
     freqs = FFTW.fftfreq(length(t), 1.0/dt) |> fftshift
     over_0 = findall(freqs .> 0);
-    n_sweep, n_data, n_ch = size(rod_data_B)
+    n_sweep, n_data, n_ch = size(data)
 	fft_data = zeros(Complex, n_sweep, n_data, n_ch)
 	for swp in 1:n_sweep
 		for ch in 1:n_ch
-			fft_data[swp, :, ch] = fft(rod_data_B.data_array[swp, :, ch]) |> fftshift
+			fft_data[swp, :, ch] = fft(data.data_array[swp, :, ch]) |> fftshift
 		end
 	end
     return freqs[over_0], fft_data[:, over_0, :] 
