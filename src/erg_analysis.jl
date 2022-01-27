09487e9b10f3b9cb0c2a0ea087476cb9948902ac
Matthew Tarchick <mtarchick13@gmail.com>
2022-01-27 12:43:13 -0500

    added Recovery and Amplification models

--- src/erg_analysis.jl
+++ src/erg_analysis.jl
@@ -115,6 +115,17 @@ function integral(data::Experiment{T}) where {T<:Real}
 end
 
 # The below functions are created by fitting a model 
+"""
+# Recovery Time Constant (τRec)
+
+This function is a single exponential. 
+
+### Function usage
+[IN 1]:  Recovery(t, V⁰, τRec)
+
+[OUT 1]: Response  
+"""
+REC(t, V⁰, τRec) = V⁰ * exp(-t / τRec)
 
 """
 The dominant time constant is calculated by fitting the normalized Rdim with the response recovery equation
@@ -166,8 +177,32 @@ function recovery_tau(data::Experiment{T}, resp::Union{T,Matrix{T}};
 end
 
 #Function to fit Amplification
+"""
+# Amplification 
 
+Amplification is a time series, therefore it is a function of time
+
+## The relationship is demonstrated by
+\$R = f(t)\$
+
+\$f(t) = R_{max}(1-e^{-\\alpha(t-t_{eff})^2})\$
+
+### Variables
+- R: The response is the dependent variable
+- t: Time is the independent variable.
+
+### Parameters
+- (\$t_{eff}\$): The effective time delay is a short delay between stimulus onset and response onset indicative of the biomolecuar diffusion rates
+- (\$\\alpha\$): The amplification coefficient  represents the rate of the response increases from the biomolecular processes. 
+
+### Function usage
+[IN 1]:  AMP(t, α, t_eff, rmax)
+
+[OUT 1]: Response
+
+"""
 AMP(t, α, t_eff, rmax) = t > t_eff ? rmax * (1 - exp(-α * (t - t_eff)^2)) : 0.0
+
 function amplification(data::Experiment{T}, resp::Union{T,Matrix{T}}; #This argument should be offloaded to a single value 
     time_cutoff = 0.1,
     lb::Vector{T} = [0.0, 0.001],
