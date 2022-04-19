089e81808cf77ab4331f5d9c92a554e0b84d314e
Matthew Tarchick <mtarchick13@gmail.com>
2022-04-19 11:31:25 -0400

    updated integral equation

--- src/erg_analysis.jl
+++ src/erg_analysis.jl
@@ -107,12 +107,15 @@ pepperburg_analysis(data::Experiment{T}; kwargs...) where {T<:Real} = pepperburg
 The integration time is fit by integrating the dim flash response and dividing it by the dim flash response amplitude
 - A key to note here is that the exact f(x) of the ERG data is not completely known
 - The integral is therefore a defininte integral and a sum of the area under the curve
+- This equation is according to 
+    Baylor DA, Hodgkin AL (1973) Detection and resolution of visual stimuli
+        by turtle photoreceptors. J Physiol 234:163–198.
 """
-
 function integral(data::Experiment{T}) where {T<:Real}
     #we want this to be equal to any response after the stimuli
-    t_points = findall(data.t .>= 0.0)
-    data_section = data[:, t_points, :]
+    data_section = data[:, data.t.>0.0, :]
+    data_section = abs.(data_section)
+    data_section ./= maximum(data_section, dims = 2)
     return sum(data_section, dims = 2) * data.dt
 end
 
