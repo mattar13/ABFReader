fbf329140edbea3c06beca50c010d2c992d9b2c5
Matthew Tarchick <mtarchick13@gmail.com>
2022-06-20 14:48:34 -0400

    removed extra packages

--- src/ABFReader.jl
+++ src/ABFReader.jl
@@ -1,21 +1,7 @@
 module ABFReader
 
-#lets go through all the imports and remove the unnecessary ones we have moved out of this file
-pkg_dir = joinpath(splitpath(pathof(ABFReader))[1:end-2]...)
-export pkg_dir
-
 using Base: String, println
 using Dates
-#Imports
-#using Statistics
-#using Polynomials
-#using Distributions
-#using StatsBase #Used for polynomial fitting
-#using LsqFit #Used for fitting amplification and Intensity Response models
-#using DSP
-#using ContinuousWavelets
-#using Wavelets
-#using FFTW #Used for filtering
 
 #Functions that can help with file extraction
 include("ABFExtraction/ByteMaps.jl") #These functions deal with the bytemap extractions
